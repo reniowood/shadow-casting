fa7cc3cab961380651d4c3b74ab15d9b9cc582d6
Jinhyuk Kim <reniowood@gmail.com>
2020-05-02 14:17:47 -0700

    Add edges and draw edges

--- src/canvas.ts
+++ src/canvas.ts
@@ -15,6 +15,7 @@ export default class Canvas {
     this.element.onclick = (event) => {
       console.log(`clicked: (${event.offsetX}, ${event.offsetY})`);
       map.toggle(Math.floor(event.offsetY / 16), Math.floor(event.offsetX / 16));
+      map.updateEdges();
     };
     this.map = map;
     this.scale = {
@@ -34,15 +35,60 @@ export default class Canvas {
   }
 
   draw() {
+    this.clearCanvas();
+    this.drawCell();
+    this.drawEdges();
+  }
+
+  private clearCanvas() {
+    const context = this.element.getContext('2d');
+
+    context.clearRect(0, 0, this.width, this.height);
+  }
+
+  private drawCell() {
     const context = this.element.getContext('2d');
 
     for (let i = 0; i < this.height; i += this.scale.y) {
       for (let j = 0; j < this.width; j += this.scale.x) {
-        if (this.map.get(Math.floor(i / this.scale.x), Math.floor(j / this.scale.y))) {
+        if (this.map.isFilled(Math.floor(i / this.scale.x), Math.floor(j / this.scale.y))) {
           context.fillStyle = "#0000ff";
           context.fillRect(j, i, this.scale.x, this.scale.y);
         }
       }
     }
+  }
+
+  private drawEdges() {
+    this.map.edges.forEach((edge) => {
+      const radius = Math.min(this.scale.x, this.scale.y) / 4;
+      const x1 = edge.x1 * this.scale.x;
+      const y1 = edge.y1 * this.scale.y;
+      const x2 = edge.x2 * this.scale.x;
+      const y2 = edge.y2 * this.scale.y;
+
+      this.drawPoint(x1, y1, radius);
+      this.drawLine(x1, y1, x2, y2);
+      this.drawPoint(x2, y2, radius);
+    });
+  }
+
+  private drawPoint(x: number, y: number, r: number) {
+    const context = this.element.getContext('2d');
+
+    context.beginPath();
+    context.arc(x, y, r, 0, 2 * Math.PI);
+    context.fillStyle = "#ff0000";
+    context.fill();
+    context.stroke();
+  }
+
+  private drawLine(x1: number, y1: number, x2: number, y2: number) {
+    const context = this.element.getContext('2d');
+
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
   }
 }
--- src/map.ts
+++ src/map.ts
@@ -1,5 +1,21 @@
+interface Cell {
+  filled: boolean;
+  up?: Edge;
+  down?: Edge;
+  left?: Edge;
+  right?: Edge;
+}
+
+interface Edge {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
 export default class Map {
-  private readonly map: boolean[][];
+  private readonly map: Cell[][];
+  edges: Edge[];
   readonly width: number;
   readonly height: number;
 
@@ -7,29 +23,125 @@ export default class Map {
     this.width = width;
     this.height = height;
     this.map = this.createMap(width, height);
+    this.edges = [];
   }
 
-  private createMap(width: number, height: number): boolean[][] {
-    const map: boolean[][] = [];
+  isFilled(y: number, x: number): boolean {
+    return this.map[y][x].filled;
+  }
+
+  toggle(y: number, x: number) {
+    this.map[y][x].filled = !this.map[y][x].filled;
+  }
+
+  updateEdges() {
+    this.edges = [];
+
+    for (let y = 0; y < this.height; y += 1) {
+      for (let x = 0; x < this.width; x += 1) {
+        this.map[y][x] = {
+          filled: this.map[y][x].filled,
+        };
+
+        if (this.map[y][x].filled) {
+          this.checkTopCell(y, x);
+          this.checkBottomCell(y, x);
+          this.checkLeftCell(y, x);
+          this.checkRightCell(y, x);
+        }
+      }
+    }
+  }
+
+  private checkTopCell(y: number, x: number) {
+    if (y - 1 >= 0 && !this.map[y - 1][x].filled) {
+      if (x - 1 >= 0 && this.map[y][x - 1].up) {
+        const edge = this.map[y][x - 1].up;
+        edge.x2 += 1;
+        this.map[y][x].up = edge;
+      } else {
+        const edge: Edge = {
+          x1: x,
+          y1: y,
+          x2: x + 1,
+          y2: y
+        };
+        this.map[y][x].up = edge;
+        this.edges.push(edge);
+      }
+    }
+  }
+
+  private checkBottomCell(y: number, x: number) {
+    if (y + 1 < this.height && !this.map[y + 1][x].filled) {
+      if (x - 1 >= 0 && this.map[y][x - 1].down) {
+        const edge = this.map[y][x - 1].down;
+        edge.x2 += 1;
+        this.map[y][x].down = edge;
+      } else {
+        const edge: Edge = {
+          x1: x,
+          y1: y + 1,
+          x2: x + 1,
+          y2: y + 1
+        };
+        this.map[y][x].down = edge;
+        this.edges.push(edge);
+      }
+    }
+  }
+
+  private checkLeftCell(y: number, x: number) {
+    if (x - 1 >= 0 && !this.map[y][x - 1].filled) {
+      if (y - 1 >= 0 && this.map[y - 1][x].left) {
+        const edge = this.map[y - 1][x].left;
+        edge.y2 += 1;
+        this.map[y][x].left = edge;
+      } else {
+        const edge: Edge = {
+          x1: x,
+          y1: y,
+          x2: x,
+          y2: y + 1
+        };
+        this.map[y][x].left = edge;
+        this.edges.push(edge);
+      }
+    }
+  }
+
+  private checkRightCell(y: number, x: number) {
+    if (x + 1 < this.width && !this.map[y][x + 1].filled) {
+      if (y - 1 >= 0 && this.map[y - 1][x].right) {
+        const edge = this.map[y - 1][x].right;
+        edge.y2 += 1;
+        this.map[y][x].right = edge;
+      } else {
+        const edge: Edge = {
+          x1: x + 1,
+          y1: y,
+          x2: x + 1,
+          y2: y + 1
+        };
+        this.map[y][x].right = edge;
+        this.edges.push(edge);
+      }
+    }
+  }
+
+  private createMap(width: number, height: number): Cell[][] {
+    const map: Cell[][] = [];
 
     for (let i = 0; i < height; i += 1) {
-      const row = [];
+      const row: Cell[] = [];
 
       for (let j = 0; j < width; j += 1) {
-        row.push(false);
+        row.push({ filled: false });
       }
 
       map.push(row);
     }
 
     return map;
-  }
-
-  get(y: number, x: number): boolean {
-    return this.map[y][x];
-  }
-
-  toggle(y: number, x: number) {
-    this.map[y][x] = !this.map[y][x];
   }
 }
